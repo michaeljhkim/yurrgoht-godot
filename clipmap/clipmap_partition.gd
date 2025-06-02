11daec8a3d5d353efb6621659309b35618461b06
michaeljhkim <michaelkim291@gmail.com>
2025-06-02 14:34:53 -0400

    finished basic mesh generation

--- clipmap/clipmap_partition.gd
+++ clipmap/clipmap_partition.gd
@@ -1,12 +1,23 @@
 extends MeshInstance3D
 
-var x
-var z
+var x = 0
+var z = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	var length = ProjectSettings.get_setting("shader_globals/clipmap_partition_length").value
+	var lod_step = ProjectSettings.get_setting("shader_globals/lod_step").value
+	
+	mesh = PlaneMesh.new()
+	mesh.size = Vector2.ONE * length
+	
+	position = Vector3(x, 0, z) * length
+	
+	var lod = max(abs(x), abs(z)) * lod_step
+	var subdivision_length = pow(2, lod)
+	var subdivides = max(mesh.size.x/subdivision_length - 1, 0)
+	mesh.subdivide_width = subdivides
+	mesh.subdivide_depth = subdivides
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
